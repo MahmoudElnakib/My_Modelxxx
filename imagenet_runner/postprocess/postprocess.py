571eaefcb7e761c732a423d2912f057158211ab9
MahmoudElnakib <103126461+MahmoudElnakib@users.noreply.github.com>
2023-08-19 15:20:05 +0200

    Update postprocess.py

--- imagenet_runner/postprocess/postprocess.py
+++ imagenet_runner/postprocess/postprocess.py
@@ -1,70 +1,62 @@
-import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-from PIL import Image
-import numpy as np
-
 import pycocotools.mask as mask_util
 import cv2
 
-classes = [line.rstrip('\n') for line in open('coco_classes.txt')]
+%matplotlib inline
 
-# You might need to adjust score threshold
-def display_objdetect_image(image, boxes, labels, scores, masks, score_threshold=0.7):
-    # Resize boxes
-    ratio = 800.0 / min(image.size[0], image.size[1])
-    boxes /= ratio
+import numpy as np
+from PIL import Image, ImageDraw, ImageColor
+import math
+import matplotlib.pyplot as plt
 
-    _, ax = plt.subplots(1, figsize=(12,9))
+coco_classes = {
+    1: 'Cyclist',
+    2: 'DontCare',
+    3: 'Misc',
+    4: 'Person_sitting',
+    5: 'Tram',
+    6: 'Truck',
+    7: 'Van',
+    8: 'car',
+    9: 'person',
+}
 
-    image = np.array(image)
-
-    for mask, box, label, score in zip(masks, boxes, labels, scores):
-        # Showing boxes with score > 0.7
-        if score <= score_threshold:
-            continue
-
-        # Finding contour based on mask
-        mask = mask[0, :, :, None]
-        int_box = [int(i) for i in box]
-        mask = cv2.resize(mask, (int_box[2]-int_box[0]+1, int_box[3]-int_box[1]+1))
-        mask = mask > 0.5
-        im_mask = np.zeros((image.shape[0], image.shape[1]), dtype=np.uint8)
-        x_0 = max(int_box[0], 0)
-        x_1 = min(int_box[2] + 1, image.shape[1])
-        y_0 = max(int_box[1], 0)
-        y_1 = min(int_box[3] + 1, image.shape[0])
-        mask_y_0 = max(y_0 - box[1], 0)
-        mask_y_1 = mask_y_0 + y_1 - y_0
-        mask_x_0 = max(x_0 - box[0], 0)
-        mask_x_1 = mask_x_0 + x_1 - x_0
-        im_mask[y_0:y_1, x_0:x_1] = mask[
-            mask_y_0 : mask_y_1, mask_x_0 : mask_x_1
-        ]
-        im_mask = im_mask[:, :, None]
-
-        # OpenCV version 4.x
-        contours, hierarchy = cv2.findContours(
-            im_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
-        )
-
-        image = cv2.drawContours(image, contours, -1, 25, 3)
-
-        rect = patches.Rectangle((box[0], box[1]), box[2] - box[0], box[3] - box[1], linewidth=1, edgecolor='b', facecolor='none')
-        ax.annotate(classes[label] + ':' + str(np.round(score, 2)), (box[0], box[1]), color='w', fontsize=12)
-        ax.add_patch(rect)
-
-    ax.imshow(image)
+def draw_detection(draw, d, c):
+    """Draw box and label for 1 detection."""
+    width, height = draw.im.size
+    # the box is relative to the image size so we multiply with height and width to get pixels.
+    top = d[0] * height
+    left = d[1] * width
+    bottom = d[2] * height
+    right = d[3] * width
+    top = max(0, np.floor(top + 0.5).astype('int32'))
+    left = max(0, np.floor(left + 0.5).astype('int32'))
+    bottom = min(height, np.floor(bottom + 0.5).astype('int32'))
+    right = min(width, np.floor(right + 0.5).astype('int32'))
+    label = coco_classes[c]
+    label_size = draw.textsize(label)
+    if top - label_size[1] >= 0:
+        text_origin = tuple(np.array([left, top - label_size[1]]))
+    else:
+        text_origin = tuple(np.array([left, top + 1]))
+    color = ImageColor.getrgb("red")
+    thickness = 0
+    draw.rectangle([left + thickness, top + thickness, right - thickness, bottom - thickness], outline=color)
+    draw.text(text_origin, label, fill=color)  # , font=font)
+    
     plt.savefig("annotated.png")
 
+batch_size = num_detections.shape[0]
+draw = ImageDraw.Draw(img)
+for batch in range(0, batch_size):
+    for detection in range(0, int(num_detections[batch])):
+        c = detection_classes[batch][detection]
+        d = detection_boxes[batch][detection]
+        score = detection_scores[batch][detection]
+        if score > 0.1:
+            draw_detection(draw, d, c)
 
-
-
-img = Image.open('../images/preprocessed.jpg')
-
-labels = np.fromfile("../labels.data", dtype=np.int64)
-NUM_CLASSES = labels.size
-boxes = np.fromfile("../boxes.data", dtype=np.float32).reshape((NUM_CLASSES, 4))
-scores = np.fromfile("../scores.data", dtype=np.float32).reshape((NUM_CLASSES))
-# For model exported from pytorch use 800x800
-masks = np.fromfile("../masks.data", dtype=np.float32).reshape((NUM_CLASSES, 1, 28, 28))
-display_objdetect_image(img, boxes, labels, scores, masks)
+plt.figure(figsize=(80, 40))
+plt.axis('off')
+plt.imshow(img)
+plt.show()
